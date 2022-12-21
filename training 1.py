0e1673a54fdd5f3f7418230e5fd2e1534803ad1f
LEX1005001 <AleX69.galkin@yandex.ru>
2022-12-21 19:22:35 +0300

    Class Camera

--- training 1.py
+++ training 1.py
@@ -1,16 +1,16 @@
-class Cup:
-    def __init__(self,material) :
-        self.material=material
-        self.amount_liquid=0
-    def increase(self,amount:int)->int:
-        self.amount_liquid+=amount
-    def decrease(self,damount:int):
-        self.amount_liquid-=damount
-        if self.amount_liquid<0:
-            self.amount_liquid=0
-    def inf(self):
-        print(f"Кружка из {self.material}\nкол-во вещества в кружке<{self.amount_liquid}мл>")
-p=Cup("Стекляная")
-p.increase(4)
-p.decrease(2)
-p.inf()
+from random import randint
+class Camera:
+    def __init__(self,name) -> None:
+        self.name=name
+        self.snapshots=0
+        self.album=[]
+    def take_photo(self):
+        self.snapshots+=1
+        self.album.append(1==randint(0,1))
+    def info(self):
+        print(f"Название камеры <{self.name}>\nКол-во снимков <{self.snapshots}>\nАльбом фотографий->{self.album}")
+cam1=Camera("Rock")
+cam1.take_photo()
+cam1.take_photo()
+cam1.take_photo()
+cam1.info()
